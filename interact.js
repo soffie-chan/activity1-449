96e90b2510c285b60afcfaede2ee4b70f9589f32
Soffie <soffie@udel.edu>
2026-02-06 02:38:31 -0500

    bark adjusted with flex and works in js. all done!

--- interact.js
+++ interact.js
@@ -1,12 +1,13 @@
 
 const button = document.getElementById("button");
 const hand = document.getElementById("hand");
+const bark = document.getElementById("bark");
 
 button.addEventListener("mousedown",()=>{
-    hand.style.top = "180px";
-
+    hand.style.top = "190px";
+    bark.style.visibility = "visible";
 });
 button.addEventListener("mouseup",()=>{
     hand.style.top = "0px";
-
+    bark.style.visibility = "hidden";
 });
